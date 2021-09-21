11fd51f803ca459607bb3bfe977167e8e89307b9
Keshava Varma <keshavavarma.ns@gmail.com>
2021-09-21 13:32:08 +0530

    component library file structure

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
 function buildStyles() {
-  return src("index.scss").pipe(sass()).pipe(dest("css"));
+  return src("simpleUI/**/*.scss").pipe(sass()).pipe(dest("css"));
 }
 
 function watchTask() {
-  watch(["index.scss"], buildStyles);
+  watch(["simpleUI/**/*.scss"], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
